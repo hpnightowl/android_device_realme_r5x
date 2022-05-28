dfb19f75a8a3f6cf6467a76f40b47d82947e1b4e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:11:13 +0200

    r5x: Remove TARGET_ENABLE_MEDIADRM_64
    * It's only used when building drm@1.0 HIDL
    
    Change-Id: I06412e2dd2bb48d798be4ef6e9a26a17b65277a7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,9 +121,6 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 
 TARGET_USES_VULKAN := true
 
-# DRM
-TARGET_ENABLE_MEDIADRM_64 := true
-
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
