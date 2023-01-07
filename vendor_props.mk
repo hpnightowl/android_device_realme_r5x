0378f21d3a8753f93474a874d9c697c02a651e78
Edwin Moquete <edwinmmoquete@gmail.com>
2023-01-07 09:28:14 +0000

    r5x: Remove unused persist.bluetooth.a2dp_offload.cap property
    This prop has no effect as of T QPR1.
    
    See: https://github.com/LineageOS/android_packages_modules_Bluetooth/commit/cf25e331479a850ad888f077b1d6bac7419213e4
    
    Change-Id: Ic762812dd59429d344ccc55c01bf96b0ffd6dbab

--- vendor_props.mk
+++ vendor_props.mk
@@ -91,7 +91,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
     persist.bluetooth.a2dp_offload.disabled=false \
     persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
     persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false \
