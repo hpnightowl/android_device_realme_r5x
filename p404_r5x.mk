22b44ab2f0f1894d2c1de351bf669f96fc909363
hpnightowl <iamhp2k@gmail.com>
2023-04-03 00:17:24 +0530

    r5x: move to vendor/404

--- p404_r5x.mk
+++ p404_r5x.mk
@@ -6,7 +6,6 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from the 404 configuration.
@@ -31,12 +30,3 @@ TARGET_VENDOR_DEVICE_NAME := r5x
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="r5x" \
     PRIVATE_BUILD_DESC="trinket-user 10 QKQ1.200209.002 release-keys"
-
-# P404 Stuff
-P404_BUILDTYPE := TOKUI
-
-ifeq ($(WITH_GAPPS),true)
-P404_BUILDTYPE := TOKUI-GAPPS
-else
-P404_BUILDTYPE := TOKUI-VANILLA
-endif
