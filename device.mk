ebbf697693589c2676b6cf2de849bbfabd029d22
Harsh  Pal <iamhp2k@gmail.com>
2023-04-05 12:19:47 +0000

    r5x: drop light service

--- device.mk
+++ device.mk
@@ -241,10 +241,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.lights-service.r5x
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml
